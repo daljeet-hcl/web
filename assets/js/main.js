3925b9736da0dd05907843dcd37da580d8dbc8cd
Sarabveer Singh <me@sarabveer.me>
2018-04-01 23:07:56 -0400

    Save SearchType in session storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+if (sessionStorage.getItem('s.data.searchtype') === null) {
+	sessionStorage.setItem('s.data.searchtype', 0);
+}
+
 function showresult(string, ontype) {
 	if (string.length === 0) {
 		document.getElementById("searchinfo").textContent = "Search Box is Empty!";
@@ -79,102 +83,42 @@ function showresult(string, ontype) {
 	}
 }
 
-function showang(angnum) {
-	document.getElementById("searchresults").innerHTML = "";
-	document.getElementById("searchinfo").textContent = "Searching...";
-	var source = document.getElementById("sourceoption").value;
-	var request = new XMLHttpRequest();
-	var url = GurbaniNow.buildApiUrl({
-		ang: angnum,
-		source: source
-	});
-	request.open("GET", url, true);
-	request.onload = function() {
-		if (this.status >= 200 && this.status < 400) {
-			var data = JSON.parse(this.response);
-			var searchcount = data.count;
-			data.page.forEach(function(lines, i) {
-				if (data.source.id == "G") {
-					var ang = "Ang";
-				} else if (data.source.id == "D" || data.source.id == "A" || data.source.id == "U") {
-					var ang = "Panaa";
-				} else {
-					var ang = "Vaar";
-				}
-				if (data.source.id == "G") {
-					var source = " (SGGS)";
-				} else if (data.source.id == "D") {
-					var source = " (Sri Dasam Granth)";
-				} else {
-					var source = "";
-				}
-				if (lines.line.writer.english === "") {
-					var writer = lines.line.raag.english;
-				} else {
-					var writer = lines.line.writer.english;
-				}
-				var append = document.createElement("div");
-				append.classList.add("list-group");
-				var html = '<a href="/shabad/' + lines.line.shabadid + '/' + lines.line.id + '" class="list-group-item">';
-				html += '<h3 class="list-group-item-heading" style="font-family: GurbaniAkharThick; color: #ffffff;">' + lines.line.gurmukhi.akhar + '</h3>';
-				html += '<p class="list-group-item-text"><small>' + lines.line.translation.english.default+'</small></p>';
-				html += '<p class="list-group-item-text"><small><b>' + lines.line.raag.english + ', ' + writer + ', ' + ang + ' ' + lines.line.pageno + source + '</b></small></p>';
-				html += "</a>";
-				append.innerHTML = html;
-				document.getElementById("searchresults").appendChild(append);
-			});
-			if (searchcount === 0) {
-				var resultText = "No Page Found! Please Check Your Input";
-			} else if (searchcount === 1) {
-				var resultText = "Your Search Returned 1 Line on Page " + angnum;
-			} else {
-				resultText = "Your Search Returned " + searchcount + " Lines on Page " + angnum;
-			}
-			document.getElementById("searchinfo").textContent = resultText;
-		} else {
-			document.getElementById("searchinfo").textContent = "An Error has occurred, Try Again.";
-		}
-	};
-	request.onerror = function() {
-		document.getElementById("searchinfo").textContent = "An Error has occurred, Try Again.";
-	};
-	request.send();
-}
-
-function searchtype() {
-	var selectedvaluetype = document.getElementById("searchtypeoption").value;
-	if (selectedvaluetype == "0") {
+function setsourceinfo(selectedvalue) {
+	if (selectedvalue == "0") {
 		var type = "Gurmukhi - First Letter (Start)";
-	} else if (selectedvaluetype == "1") {
+	} else if (selectedvalue == "1") {
 		var type = "Gurmukhi - First Letter (Anywhere)";
-	} else if (selectedvaluetype == "2") {
+	} else if (selectedvalue == "2") {
 		var type = "Gurmukhi - Full Word/Line";
-	} else if (selectedvaluetype == "3") {
+	} else if (selectedvalue == "3") {
 		var type = "English - Full Word/Line";
-	} else if (selectedvaluetype == "4") {
+	} else if (selectedvalue == "4") {
 		var type = "Gurmukhi - Search All Words";
-	} else if (selectedvaluetype == "5") {
+	} else if (selectedvalue == "5") {
 		var type = "English - Search All Words";
-	} else if (selectedvaluetype == "6") {
+	} else if (selectedvalue == "6") {
 		var type = "Gurmukhi - Search Any Words";
-	} else if (selectedvaluetype == "7") {
+	} else if (selectedvalue == "7") {
 		var type = "English - Search Any Words";
 	}
 	document.getElementById("searchinfo").textContent = "Search: " + type;
-}
-
-function searchsetmsg() {
-	var selectedvalue = document.getElementById("searchtypeoption").value;
 	if (selectedvalue == "0" || selectedvalue == "1" || selectedvalue == "2" || selectedvalue == "4" || selectedvalue == "6") {
 		document.getElementById("searchbox").setAttribute("style", "font-family: GurbaniAkharThick; font-size: 1.2em;");
-		document.getElementById("searchbox").setAttribute("placeholder", "ਖੋਜ...");
+		document.getElementById("searchbox").setAttribute("placeholder", "koj...");
 	}
 	if (selectedvalue == "3" || selectedvalue == "5" || selectedvalue == "7") {
 		document.getElementById("searchbox").setAttribute("style", "");
 		document.getElementById("searchbox").setAttribute("placeholder", "Search...");
 	}
+	sessionStorage.setItem('s.data.searchtype', selectedvalue);
 }
-setInterval(searchsetmsg, 1000);
+
+function updatesourceinfo() {
+	var searchtype = sessionStorage.getItem('s.data.searchtype');
+	if (searchtype != 0) {
+		setsourceinfo(document.getElementById("searchtypeoption").value);
+	}
+}
 
 function clearbtn() {
 	document.getElementById("searchbox").value = "";
@@ -233,3 +177,5 @@ document.querySelectorAll("#gurmukhi-keyboard button").forEach(function(el) {
 		}
 	});
 });
+
+setInterval(updatesourceinfo, 500);
